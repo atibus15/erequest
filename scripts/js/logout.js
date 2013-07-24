da18b77af4c8a982fd552fb1dd02de0c62651dab
atibus15 <adamson.ibus@gmail.com>
2013-07-24 10:38:17 +0800

    Update logout.js

--- scripts/js/logout.js
+++ scripts/js/logout.js
@@ -1,9 +1,4 @@
-/* 
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- * @author  : jrdumayag
- * @date    : 05-29-2010
- */
+
 Ext.onReady(function(){
 
     var logout = Ext.get('logout');
